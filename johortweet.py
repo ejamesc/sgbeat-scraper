fa2e324f637bf3414ce20178e723624346af3f70
Eli James <ejames@pandamian.com>
2012-03-14 22:00:12 +0800

    Added johortweet.py, uses Twitter's streaming API to grab tweets.

---
+++ johortweet.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""Use Twitter's streaming API to grab tweets.
+"""
+
+import tweetstream
+from details import USERNAME, PASSWORD
+
+#note that coordinate pairs are long/lat not lat/long
+#Singapore coordinates
+#locations = ["103.61,1.22","104.01356,1.456674"]
+
+#Johor coordinates
+locations = ["103.55,1.45", "103.87,1.63"]
+
+
+with tweetstream.FilterStream(
+				USERNAME,
+				PASSWORD,
+				locations=locations) as stream:
+	for tweet in stream:
+		print "==================="
+		print tweet["text"]
+		print tweet["user"]["screen_name"]
+		print "(%s)" % tweet["place"]["full_name"]
+
