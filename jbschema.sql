2630ff32928f81b46fd36a0f4aece9c62fba45af
Eli James <ejames@pandamian.com>
2012-03-15 17:11:15 +0800

    Updated Singaporean script

--- jbschema.sql
+++ jbschema.sql
@@ -8,6 +8,6 @@ DROP TABLE IF EXISTS tweets;
 CREATE TABLE tweets (
 	id int(8) primary key auto_increment,
 	user int(8) REFERENCES users(id),
-	location varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-	tweet varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci not null
+	location varchar(128) CHARACTER SET utf8 DEFAULT NULL,
+	tweet varchar(256) CHARACTER SET utf8 not null
 );
